2e2c231023d083bbeeaa8ec96c38cbc6a17b98c7
yusufmafif <yusufmafif@gmail.com>
2024-03-18 00:10:12 +0700

    Bismillah

--- app/(home)/blog/[id]/deleteComment.tsx
+++ app/(home)/blog/[id]/deleteComment.tsx
@@ -8,11 +8,9 @@ const DeleteComment = (blog: any) => {
     const user = useUser((state) => state.user)
     const deleteComment = (blog: any) => {
         deleteCommentById(blog.blog.uuid)
-    //  alert(blog.blog.uuid)
-    //   alert(blog.blog.user_id)
+
     }
-    // const isAuthorized = user?.id === blog.blog.user_id
-    const isAuthorized = "76d0ed3c-caad-4ab4-ba09-ebb1f367abed" === blog.blog.user_id
+    const isAuthorized = user?.id === blog.blog.user_id
     return (
         <div>
             {isAuthorized && <form ><Button onClick={() => deleteComment(blog)} className='flex items-center gap-2'>Delete</Button></form>}
